00ab47e3034549639b22acc18c7a4c3508257a03
Christian Sieber <c.sieber@tum.de>
2016-04-29 15:44:45 +0200

    fixed figure

--- python/initialdelay.py
+++ python/initialdelay.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         y = ecdf(x)
         plt.step(x, y, color=next(ic))                                       
                 
-    plt.xlim([0, 8])                
+    plt.xlim([0, 250])                
     plt.ylim([0, 1])                
                 
     plt.xlabel("Initial Delay (s)")
@@ -29,8 +29,8 @@ if __name__ == "__main__":
     
     #plt.annotate(s=r'optimization', xy=(30,1.6), xytext=(15,2.2), arrowprops=dict(arrowstyle='->'))         
     
-    plt.annotate(s='opt (no stall)', xy=(3,0.65), xytext=(1,0.8), arrowprops=dict(arrowstyle='->'))     
-    plt.annotate(s='opt (stalling)', xy=(6,0.2), xytext=(4,0.4), arrowprops=dict(arrowstyle='->'))     
+    plt.annotate(s='opt (no stall)', xy=(10,0.95), xytext=(80,0.8), arrowprops=dict(arrowstyle='->'))     
+    plt.annotate(s='opt (stalling)', xy=(15,0.3), xytext=(50,0.4), arrowprops=dict(arrowstyle='->'))     
     
     texSaveFig("../latex/figs/initial_delay_py.pdf")    
     plt.close(fig)
